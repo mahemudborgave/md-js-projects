cdbad57cad83b99d3d67c1fa41043880b40ce1da
Md <mahemudborgave@gmail.com>
2024-08-07 08:46:22 +0530

    trying to fix error being shown in console

--- WeatherApp/index.js
+++ WeatherApp/index.js
@@ -11,22 +11,38 @@ let wind = document.querySelector(".wind-speed");
 
 sButton.addEventListener("click", async function getData() {
   cityc = inputCity.value;
+  if (cityc === "") {
+    alert("Please enter city !");
+    return;
+  }
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityc}&appid=d9b9ac632bdc802b30dedf49142654eb`;
-  let response = await fetch(url);
+  let response;
+  let data;
 
-  const data = await response.json();
-  console.log(data);
+  try {
+    response = await fetch(url);
 
-  city.innerHTML = data.name;
-  temp.innerHTML = Math.round((data.main.temp - 273.15).toFixed(2)) + "°C";
-  humidity.innerHTML = data.main.humidity + "%";
-  wind.innerHTML = data.wind.speed + "km/h";
+    if (!response.ok) {
+      throw new Error("Error while fetching data !");
+    }
+    data = await response.json();
 
-  if (Math.round((data.main.temp - 273.15).toFixed(2)) < 25) {
-    weatherImg.src = "./images/rain.png";
-  } else if (Math.round((data.main.temp - 273.15).toFixed(2)) < 30) {
-    weatherImg.src = "./images/clouds.png";
-  } else {
-    weatherImg.src = "./images/clear.png";
+    console.log(data);
+
+    city.innerHTML = data.name;
+    temp.innerHTML = Math.round((data.main.temp - 273.15).toFixed(2)) + "°C";
+    humidity.innerHTML = data.main.humidity + "%";
+    wind.innerHTML = data.wind.speed + " km/h";
+
+    if (Math.round((data.main.temp - 273.15).toFixed(2)) < 25) {
+      weatherImg.src = "./images/rain.png";
+    } else if (Math.round((data.main.temp - 273.15).toFixed(2)) < 30) {
+      weatherImg.src = "./images/clouds.png";
+    } else {
+      weatherImg.src = "./images/clear.png";
+    }
+  } catch (err) {
+    console.log("Error: " + err.message);
+    return;
   }
 });
